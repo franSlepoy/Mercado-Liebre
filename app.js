55bae6b060c3d504bd5ebf2b25c2483ca1869b56
Carmen <carmen@MacBook-Air-de-Francisco.local>
2023-01-05 00:12:12 -0300

    cambios para render

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require("path")
 app.use(express.static('public'));
 
 
-app.listen(3000, () => {
-    console.log('Servidor funcionando');
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () =>console.log("Servidor corriendo"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/home.html'))
